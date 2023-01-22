e03d63da8b674e7153bc58feb9e17025810c578b
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-01-22 14:31:31 +0000

    [add]

---
+++ app/models/order.rb
@@ -0,0 +1,9 @@
+class Order < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,62 @@
 Rails.application.routes.draw do
+  devise_for :order_items
+  devise_for :orders
+  devise_for :cart_items
+  devise_for :genres
+  devise_for :items
+  devise_for :addresses
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'address/index'
+    get 'address/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
