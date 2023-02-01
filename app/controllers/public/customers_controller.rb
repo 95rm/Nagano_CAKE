cb4f6fca5774798d2e215722ee33b80789752402
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-02-01 08:31:06 +0000

    [add]退会機能実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,9 +3,23 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def stop
+  end
+
   def edit
     @customer = current_customer
   end
+
+  def update
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
 
   private
   def customer_params
