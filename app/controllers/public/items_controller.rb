ff644a9e8a700a7198145733965e861120401fbb
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-02-03 11:38:58 +0000

    [add]顧客側商品画面修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,4 +6,9 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
+  end
 end
