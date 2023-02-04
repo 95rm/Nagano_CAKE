bfcb5ce8d8546180f0f99519778d25564d73c77f
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-02-04 07:18:34 +0000

    [add]カート機能実装(途中)

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_item = current_customer.cart_items
+  end
+
+  def create
+     @cart_item = CartItem.new(cart_item_params)
+     @cart_item.save
+     redirect_to cart_items_path
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
   def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+      params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,8 +3,15 @@ class Public::ItemsController < ApplicationController
     @item = Item.all.page(params[:page]).per(8)
   end
 
+  def create
+    @item = Item.new(imte_params)
+    @item.save
+    redirect_to cart_items_path
+  end
+
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
   private
