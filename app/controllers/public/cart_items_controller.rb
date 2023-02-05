0074e92968c88347771f719d9ce7389feac30af6
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-02-05 10:09:46 +0000

    [add]ショッピングカート画面実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,30 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_item = current_customer.cart_items
+    @total = 0
   end
 
   def create
      @cart_item = CartItem.new(cart_item_params)
-     @cart_item.save
-     redirect_to cart_items_path
+     if @cart_item.save
+        redirect_to cart_items_path
+     else
+       @cart_item = CartItem.new
+     end
   end
 
   def update
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    @cart_item = current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
