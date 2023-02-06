17698e8a1188b5e40a743812b9a3424e7ff5a0db
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-02-06 14:29:10 +0000

    [add]カート機能実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,15 +5,27 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-     @cart_item = CartItem.new(cart_item_params)
-     if @cart_item.save
-        redirect_to cart_items_path
-     else
-       @cart_item = CartItem.new
-     end
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       @cart_item.amount += params[:cart_item][:amount].to_i
+       @cart_item.save
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.save
+    end
+    redirect_to cart_items_path
+     #@cart_item = CartItem.find_by(name: "")
+     #if @cart_item.save
+        #redirect_to cart_items_path
+     #else
+       #@cart_item = CartItem.new
+     #end
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,12 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to
   end
 
   def index
@@ -7,4 +14,9 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  def order_params
+    params.require(:item).permit(:customer_is, :shipping_postal_code, :shipping_address, :shipping_name, :postage, :total_payment, :payment_method, :status)
+  end
+
 end
